cd6766e42d3db6d0fdb3a56d2a8f62649fdc9b8c
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-11-01 22:53:41 -0600

    a

--- helpers/ruta.js
+++ helpers/ruta.js
@@ -1,3 +1,4 @@
-const ruta_server = process.env.RUTA || "http://localhost:3000";
+const ruta_server =
+	process.env.ENV === "PROD" ? process.env.RUTA : "http://localhost:3000";
 
 export default ruta_server;
